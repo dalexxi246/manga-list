287d4e8f116c3e2c7b1ddc4795fca333163309c4
wilmer.hurtado <wilmer.hurtado@grability.com>
2017-05-31 23:15:58 -0500

    creating login activity ando viewmodel

---
+++ app/src/main/java/com/wh2soft/mangalist/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.wh2soft.mangalist.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wh2soft.mangalist.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/wh2soft/mangalist/viewmodel/LoginViewModel.java
@@ -0,0 +1,7 @@
+package com.wh2soft.mangalist.viewmodel;
+
+import android.databinding.BaseObservable;
+
+public class LoginViewModel extends BaseObservable {
+
+}
